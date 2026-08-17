3d7ae67d73b501e858cc17586baca7f3b7694570
Arnav <chennavaram.2004@example.com>
2026-08-17 14:39:17 +0530

    Added Template.cpp

--- c++_concepts/Templates/template.cpp
+++ c++_concepts/Templates/template.cpp
@@ -1,75 +1,182 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <typeinfo>
 
 using namespace std;
 
-
-
-//Function template
-//T is the placeholder type
-template<typename T> //the moment compiler looks at this statement it creates a function template.
-
-void print(T value) //calling the function template 
+template <typename T> //the moment compiler looks at this statement it creates a class template.
+class Statistics
 {
-    cout << value << "\n";
-}
 
+    vector<T> digits;
 
-//class Template 1
-template<typename T>
-class Calculator
-{
-    public:
-    T add(T a, T b) //calling the function add with the same data type as the class
+public:
+    void Add(T value)
     {
-        return a+b;
+        digits.push_back(value);
     }
 
-    T subtract(T a, T b)
+    T min() const
     {
-        return a-b;
+        T minimum = digits[0];
+        for (int i = 0; i < digits.size(); i++)
+        {
+            if (digits[i] < minimum)
+            {
+                minimum = digits[i];
+            }
+        }
+
+        return minimum;
     }
 
+    T max() const
+    {
+        T maximum = digits[0];
+
+        for (int i = 0; i < digits.size(); i++)
+        {
+            if (digits[i] > maximum)
+            {
+                maximum = digits[i];
+            }
+        }
+        return maximum;
+    }
+
+    double average()
+    {
+        T sum = 0;
+        T Count = 0;
+        for (int i = 0; i < digits.size(); i++)
+        {
+            sum = digits[i];
+            Count++;
+        }
+
+        return static_cast<double>(sum) / Count;
+    }
+
+    void print()
+    {
+        for (int i = 0; i < digits.size(); i++)
+        {
+            cout << digits[i] << " ";
+
+            cout << endl;
+        }
+    }
 };
 
-
-//class Template 2
-template<typename T, int N> //the moment compiler looks at this statement it creates a class template.
-
-class Array //im trying to create an array of any type with a fixed size.
+//Full Template Specialisation
+template<typename T>
+void process(T value)
 {
-    private:
-        T m_array[N]; //this is a Type safe array and N is the size of the array
+    cout << "Processing generic type: " << value << endl;
+}
+
+template<>
+void process<int>(int value)    //means the generic template has a special version specifically to implement int.
+{
+    cout << "Processing integer: " << value << endl;
+}
+
+template<> //this means I am providing a completely specific implementation for one exact type.
+void process<string>(string value)
+{
+    cout << "Processing string: " << value << endl;
+}
+
+template<>
+void process<double>(double value)
+{
+    cout << "Processing floating point: " << value << endl;
+}
+
+//Partial Template Specialisation
+template<typename T, typename U>
+class Pair
+{
+    T first;
+    U second;
     public:
-        int GetSize()
-        {
-            return N;
-        }
+    Pair(T first, U second) : first(first), second(second)
+    {}
+    
+    void print()
+    {
+        cout << "Generic pair\n";
+    }
+};
+
+template<typename T>
+class Pair<T, int>
+{
+    T first;
+    int second;
+    public:
+    Pair(T first, int second) : first(first), second(second)
+    {}
+
+    void print()
+    {
+        cout << "Second Type is int\n";
+    }
 
 };
 
 int main()
 {
-    //Function Template instantiation
-    print(5.5);
-    print(4);
-   
-    //class Template instantiation 1
 
-    Calculator<int> calc1;
-    cout << calc1.add(4,5) << "\n";
+    //Normal template
+    Statistics<int> numbers;
+
+    numbers.Add(10);
+    numbers.Add(20);
+    numbers.Add(30);
+    numbers.Add(40);
+
+    cout << "Min: " << numbers.min() << endl;
+    cout << "Max: " << numbers.max() << endl;
+    cout << "Average: " << numbers.average()<< endl;
+
+    numbers.print();
+
+    Statistics<double> values;
+
+    values.Add(2.5);
+    values.Add(5.5);
+    values.Add(10.0);
+
+    cout << "Min: " << values.min() << endl;
+    cout << "Max: " << values.max() << endl;
+    cout << "Average: " << values.average() << endl;
+
+    values.print();
+
+
+    //Template Specialisation
     
-    Calculator<float> calc2;
-    cout << calc2.subtract(2,3) << "\n"; 
-   
+    //Full
+    process(100);
+    process(25.5);
+    process(string("Arnav"));
+    process('A');
 
-    //class Template instantiation 2
-    Array<int, 5> num1;
-    Array<string, 4> string1;
+    //Partial
 
-    cout << num1.GetSize() << "\n";
-    cout << string1.GetSize() << "\n";
+    Pair<double, string> genericPair(10.5, "Arnav");
+    genericPair.print();
 
-    
+    Pair<double, int> intPair(10.5, 100);
+    intPair.print();
+
+    Pair<string, int> stringIntPair("Age", 25);
+    stringIntPair.print();
+
+    Pair<int, double> intDoublePair(10, 25.5);
+    intDoublePair.print();
+
 
 
 
